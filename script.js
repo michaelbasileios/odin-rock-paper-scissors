e249eb96255035b7c295d6c462c669cfc3951378
Michael Baselios <126206406+michaelbasileios@users.noreply.github.com>
2023-09-09 11:12:39 -0500

    Declare function to play a round
    Code still rudimentary and unnecessarily complex. Further optimization
    and efficiency needed.

--- script.js
+++ script.js
@@ -3,9 +3,30 @@ const selections = ["Rock", "Paper", "Scissors"];
 function getComputerChoice(array) {
     return array[Math.floor(Math.random() * array.length)];
 }
-// console.log(computerSelection);
+const computerSelection = getComputerChoice(selections);
+console.log(computerSelection);
 
 //get player selection from prompt 
 const playerSelection = prompt("Make your choice: ");
 
-// const computerSelection = getComputerChoice(selections);
+//function to take two parameters from comp and player, compare them and declare winner
+function playRound(computerSelection, playerSelection) {
+    if (playerSelection === computerSelection) {
+        return `Draw! Both picked ${computerSelection}`;
+    } else if (playerSelection === `Rock` && computerSelection === `Paper`) {
+        return `You lose! Computer picked ${computerSelection}`;
+    } else if (playerSelection === `Rock` && computerSelection === 'Scissors') {
+        return `You win! Computer picked ${computerSelection}`;
+    } else if (playerSelection === `Paper` && computerSelection === `Rock`) {
+        return `You win! Computer picked ${computerSelection}`;
+    } else if (playerSelection === `Paper` && computerSelection === `Scissors`) {
+        return `You lose! Computer picked ${computerSelection}`;
+    } else if (playerSelection === `Scissors` && computerSelection === `Rock`) {
+        return `You lose! Computer picked ${computerSelection}`;
+    } else if (playerSelection === `Scissors` && computerSelection === `Paper`) {
+        return `You win! Computer picked ${computerSelection}`;
+    } else {
+        return `Invalid entry. Try again!`;
+    }
+}
+console.log(playRound(computerSelection, playerSelection));
