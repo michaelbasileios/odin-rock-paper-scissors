3854f74cbd06480496f16f803d54bb8fa35a5420
Michael Baselios <126206406+michaelbasileios@users.noreply.github.com>
2023-09-09 10:33:32 -0500

    Declare variable to store player choice

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 // Function (getComputerChoice) to make computer output a random choice from 3 options 
-const selections = ["rock", "paper", "scissors"];
+const selections = ["Rock", "Paper", "Scissors"];
 function getComputerChoice(array) {
     return array[Math.floor(Math.random() * array.length)];
 }
-const computerSelection = getComputerChoice(selections);
-console.log(computerSelection);
+// console.log(computerSelection);
+
+//get player selection from prompt 
+const playerSelection = prompt("Make your choice: ");
+
+// const computerSelection = getComputerChoice(selections);
