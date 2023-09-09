597fe5292f4d7cf0c8a6ca81d4d9d6d8f3065c01
Michael Baselios <126206406+michaelbasileios@users.noreply.github.com>
2023-09-09 12:49:03 -0500

    Add condition in playround for null and undefined values

--- script.js
+++ script.js
@@ -11,11 +11,13 @@ const playerSelection = prompt("Make your choice: ");
 
 //function to take two parameters from comp and player, compare them and declare result
 function playRound(computerSelection, playerSelection) {
-    let victoryMsg = `You win! Computer picked ${computerSelection}.`;
-    let defeatMsg = `You lose! Computer picked ${computerSelection}.`;
-    let drawMsg = `Draw! Both picked ${computerSelection}.`;
-    let invalidMsg = `Invalid entry. Try again.`
-    if (playerSelection.toLowerCase() === computerSelection.toLowerCase()) {
+    const victoryMsg = `You win! Computer picked ${computerSelection}.`;
+    const defeatMsg = `You lose! Computer picked ${computerSelection}.`;
+    const drawMsg = `Draw! Both picked ${computerSelection}.`;
+    const invalidMsg = `Invalid entry. Try again.`
+    if (playerSelection === undefined || playerSelection === null) {
+        return invalidMsg;
+    } else if (playerSelection.toLowerCase() === computerSelection.toLowerCase()) {
         return drawMsg;
     } else if (playerSelection.toLowerCase() === `rock` && computerSelection.toLowerCase() === `paper`) {
         return defeatMsg;
@@ -30,7 +32,18 @@ function playRound(computerSelection, playerSelection) {
     } else if (playerSelection.toLowerCase() === `scissors` && computerSelection.toLowerCase() === `paper`) {
         return victoryMsg;
     } else {
-        return invalidMsg;
+        return invalidMsg
     }
 }
 console.log(playRound(computerSelection, playerSelection));
+
+//function to play game of 5 rounds
+// function game() {
+//   return playRound(computerSelection, playerSelection);
+
+//     // for (let i = 1; i <= 5; i++) {
+//     //     playRound(computerSelection, playerSelection);
+//     // }
+// }
+
+// console.log(game());
